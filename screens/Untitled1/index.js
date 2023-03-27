b801c173a269f624f52157cdfa647cf4cfabb5ed
martin.delarosa@crowdbotics.com <martin.delarosa@crowdbotics.com>
2023-03-26 22:42:53 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,7 +1,17 @@
+import { rest_auth_user_retrieve } from "./../../store/demoappAPI/userDetails.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const Untitled1 = () => {
+const Untitled1 = ({
+  route
+}) => {
+  const {} = route.params || {};
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(rest_auth_user_retrieve());
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
--- store/formula/api.js
+++ store/formula/api.js
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 const formula = axios.create({
   baseURL: "http://ergast.com/api/f1",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function formula1_get_2023_driversjson_read(payload) {
-  return formula.get(`/2023/drivers.json`)
+  return formula.get(`/2023/drivers.json`);
 }
-export const apiService = { formula1_get_2023_driversjson_read }
+
+export const apiService = {
+  formula1_get_2023_driversjson_read
+};
